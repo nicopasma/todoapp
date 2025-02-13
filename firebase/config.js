4684a1b7fde076f8bb51783dcdf1140d10632cdd
nicopasma <n3pani00@students.oamk.fi>
2025-02-13 12:13:26 +0200

    Initial

---
+++ firebase/config.js
@@ -0,0 +1,20 @@
+import { initializeApp } from "firebase/app";
+import { getFirestore  } from "firebase/firestore";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyDu0XCt7wBnM1MAtdFVyuvUBZN1yQL2D7M",
+  authDomain: "todo-3ff2e.firebaseapp.com",
+  projectId: "todo-3ff2e",
+  storageBucket: "todo-3ff2e.firebasestorage.app",
+  messagingSenderId: "1096841125105",
+  appId: "1:1096841125105:web:c9978469d24e63a9ab5d50"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
+export const db = getFirestore(app);
+export const TODOS_REF = 'todos';
